5ddd7cc48148d7863d64ebd1c3f0bee23e7435be
Carlos <carlos.ed702@gmail.com>
2021-01-16 16:06:02 -0600

    Disenio -v2

--- src/app/Pages/pages.module.ts
+++ src/app/Pages/pages.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from '../app-routing.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PagesComponent } from './pages.component';
+import { PorfileComponent } from './porfile/porfile.component';
+import { TableComponent } from './table/table.component';
+import { IconsComponent } from './icons/icons.component';
+import { BlankpageComponent } from './blankpage/blankpage.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    PagesComponent
+    PagesComponent,
+    PorfileComponent,
+    TableComponent,
+    IconsComponent,
+    BlankpageComponent
   ],
   exports:[
     DashboardComponent,
-    PagesComponent
+    PagesComponent,
+    PorfileComponent,
+    TableComponent,
+    IconsComponent
   ],
   imports: [
     CommonModule,
--- src/app/Pages/pages.routes.ts
+++ src/app/Pages/pages.routes.ts
@@ -1,12 +1,19 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PorfileComponent } from './porfile/porfile.component';
+import { TableComponent } from './table/table.component';
+import { BlankpageComponent } from './blankpage/blankpage.component';
 
 const routes: Routes = [
     { path: 'dashboard', component: PagesComponent,
         children:[
-            { path: '', component: DashboardComponent }
+            { path: '', component: DashboardComponent },
+            { path: 'Porfile', component: PorfileComponent },
+            { path: 'Table', component: TableComponent },
+            { path: 'Blank', component: BlankpageComponent }
         ]
     }
 ];
